df7a30e1726d0f6898128f51ccda5169e361fee6
denizsincar29 <bomberman29ds@yandex.ru>
2023-04-21 18:54:13 +0300

    temporarily workarrounded video window to mpv

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import wx
+import mpv
 import wx.media
 import pafy
 from webbrowser import open as wopen
 from youtubesearchpython import VideosSearch
 from accessible_output2.outputs import auto
+#wx.media.MediaCtrl.DefaultBackend = wx.media.MEDIABACKEND_WMP10
 
 
 class YoutubePlayer(wx.Frame):
@@ -12,12 +14,13 @@ class YoutubePlayer(wx.Frame):
         wx.Frame.__init__(self, None, title="Youtube Player", size=(640, 480))
         self.panel = wx.Panel(self)
         self.edit_box = wx.TextCtrl(self.panel, style=wx.TE_PROCESS_ENTER, pos=(10, 10), size=(200, 20))
-        self.list_box = wx.ListCtrl(self.panel, pos=(10, 40), size=(200, 400))
+        self.list_box = wx.ListCtrl(self.panel, pos=(10, 40), size=(200, 400), style=wx.LC_SINGLE_SEL|wx.LC_REPORT)
         self.list_box.InsertColumn(0, 'Results')
-        self.media_player = wx.media.MediaCtrl(self.panel)
-        self.media_player.Bind(wx.media.EVT_MEDIA_LOADED, self.on_media_loaded)
+        #self.media_player = wx.media.MediaCtrl(self.panel)
+        #self.media_player.Bind(wx.media.EVT_MEDIA_LOADED, self.on_media_loaded)
         self.edit_box.Bind(wx.EVT_TEXT_ENTER, self.on_search)
         self.output = auto.Auto()
+        self.speak=self.output.output
         self.results={}
 
     def new_item(self, id, text):
@@ -38,22 +41,22 @@ class YoutubePlayer(wx.Frame):
         for i, item in enumerate(video_titles):
             self.new_item(i, item)
 
-        self.media_player.Stop()
+        #self.media_player.Stop()
         self.results={k: v for k, v in zip(video_titles, video_ids)}
         self.list_box.Focus(0)
-
+    '''
     def on_media_loaded(self, event):
         print("loaded")
         self.output.output("Playing")
         self.media_player.Play()
-
+    '''
     def play_video(self, video_id):
         url = f"https://www.youtube.com/watch?v={video_id}"
         video = pafy.new(url)
-        best = video.getbest()
+        best = video.getbest(preftype="mp4")
         play_url = best.url
         # wopen(play_url)
-        self.media_player.LoadURI(play_url)
+        mpv.MPV().play(play_url)
 
     def on_list_select(self, event):
         self.output.output("loading")
