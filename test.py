49e84e94eac95ae96159d11f7c7c2146c41bdfea
Deniz Sincar <dsincar29@gmail.com>
2023-10-19 13:02:55 +0300

    added testing script.

--- test.py
+++ test.py
@@ -1,33 +1,9 @@
-import time
-from pytube import YouTube, Search  # pafy and youtubesearchpython goes to hell!
-from prompt_toolkit import print_formatted_text as print, prompt
-from prompt_toolkit.completion import ThreadedCompleter, Completer, Completion
-from prompt_toolkit.validation import Validator
-from webbrowser import open as wopen
-from vlc import MediaPlayer
+import webbrowser
+import pytube
 
-
-class YTSearchCompleter(Completer):
-    def get_completions(self, document, event):
-        # with open("type.log", "w") as f: f.write(repr(document.text))
-        dt=Search(document.text)
-        return (Completion(i, start_position=-len(document.text)) for i in dt.completion_suggestions)
-
-sr=Search(prompt("Search: > ", completer=ThreadedCompleter(YTSearchCompleter()), complete_while_typing=False, complete_in_thread=False, validator=Validator.from_callable(lambda x: len(x)>3, "Error! Search must contain more than 3 characters!"), validate_while_typing=False))
-
-print(len(sr.results))
-astream=sr.results[0].streams.filter(file_extension="mp4")
-#audiostream="http://us2.internet-radio.com:8443/stream"
-#astream[-1].download()
-audiostream=astream[-1].url
-player=MediaPlayer(audiostream)
-player.play()
-time.sleep(1)
-try:
-    while player.is_playing():
-        pass
-except KeyboardInterrupt: pass
-except Exception as e:
-    raise(e)
-finally:
-    player.stop()
+yt=pytube.YouTube("https://www.youtube.com/watch?v=Jd1PvOdAdXY")
+streams=yt.streams
+print(f'total streams: {len(streams)}')
+vids=streams.filter(only_audio=False, only_video=False, file_extension="mp4")
+print(f'mp4 streams: {len(vids)}')
+webbrowser.open(vids.first().url)
